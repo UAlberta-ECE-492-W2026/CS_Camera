b57ab68fa3a468aca961160357ed8eb05df65313
Cole-Mckay <supercole11@gmail.com>
2026-03-01 23:28:45 -0700

    Added more files required for software function

---
+++ Reconstruction-Software/Camera_Emulator.m
@@ -0,0 +1,16 @@
+%==========================================================================
+% SCRIPT_NAME: Camera_Emulator.m
+% PURPOSE:     Simulates capturing an image with the compressive sensing 
+%              camera.
+% AUTHOR:      Cole Mckay (cdmckay1@ualberta.ca)
+% DATE:        March 1, 2026
+% VERSION:     0.0
+%
+% INPUTS:      
+% OUTPUTS:     
+%
+% DEPENDENCIES: 
+%==========================================================================
+% REVISION HISTORY:
+% 0.0 - Create File
+%==========================================================================
---
+++ Reconstruction-Software/load_config.m
@@ -0,0 +1,32 @@
+%==========================================================================
+% FUNCTION_NAME: load_config.m
+% PURPOSE:     Load a JSON profile from /configs folder
+% AUTHOR:      Cole Mckay (cdmckay1@ualberta.ca)
+% DATE:        March 1, 2026
+% VERSION:     0.0
+%
+% INPUTS:      
+% OUTPUTS:     
+%
+% DEPENDENCIES: 
+%==========================================================================
+% REVISION HISTORY:
+% 0.0 - Create File
+%==========================================================================
+
+function [outputArg1,outputArg2] = load_config(inputArg1,inputArg2)
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+arguments (Input)
+    inputArg1
+    inputArg2
+end
+
+arguments (Output)
+    outputArg1
+    outputArg2
+end
+
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
