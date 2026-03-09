a60822c28b203be90aa11e731c0dd0ebae6bedfb
Cole-Mckay <supercole11@gmail.com>
2026-03-08 23:31:13 -0600

    added initial capture script

--- Reconstruction-Software/Camera_Emulator.m
+++ Reconstruction-Software/Camera_Emulator.m
@@ -1,7 +1,9 @@
 %==========================================================================
 % SCRIPT_NAME: Camera_Emulator.m
+%
 % PURPOSE:     Simulates capturing an image with the compressive sensing 
 %              camera.
+%
 % AUTHOR:      Cole Mckay (cdmckay1@ualberta.ca)
 % DATE:        March 1, 2026
 % VERSION:     0.0
@@ -24,7 +26,31 @@ cfg = loadConfig('camera_settings.json');
 img = imresize(im2double(imread('cameraman.tif')), cfg.sampling_parameters.resolution);
 
 % Sampling Strategy (Mask Selection)
-
 maskList = selectMaskIndexes(getOptimalCore(cfg.sampling_parameters.resolution, ...
     cfg.sampling_parameters.core_mask_count), cfg.sampling_parameters.sampling_pct, ...
     cfg.sampling_parameters.resolution);
+
+% Simulation of capture
+
+flatIMG = img(:);
+
+dataMatrix = zeros(length(maskList), 2);
+
+for i = 1:length(maskList)
+   
+    index = maskList(i);
+    
+    mask = double(generate_walsh_mask(index, cfg.sampling_parameters.resolution));
+    mask(mask == 1) = cfg.mask_constraints.white_attenuation_pct;
+    mask(mask == -1 | mask == 0) = cfg.mask_constraints.black_leakage_pct;
+    mask = mask(:);
+
+    idealSignal = dot(flatIMG, mask);
+
+    noiseSamples = idealSignal + (cfg.sensor_noise.std_dev_counts ... 
+        * randn(cfg.sampling_parameters.samples_per_mask, 1));
+
+    avgSignal = mean(noiseSamples);
+
+    dataMatrix(i, :) = [index, round(avgSignal)];
+end
